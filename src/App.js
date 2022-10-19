3b1e37b12317e17bb01ceef4594400e02a43d2c2
SofiiCode <sofi.andand06@gmail.com>
2022-10-19 14:29:47 +0300

    instal bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <footer className="coder">
+        This project was coded by Sofiia Andrusyshyn and is open-soursed on{" "}
+        <a
+          href="https://github.com/SofiiCode/react-weather.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -3,91 +3,84 @@ import axios from "axios";
 import "./Weather.css";
 
 export default function Weather (){
+    const [city, setCity] = useState("");
+    const [temp, setTemp] = useState(null);
+    const [titlle, setTitlle] = useState("");
+    const [humidity, setHumidity] = useState(null);
+    const [desc, setDesc] = useState("");
+    const [wind, setWind] = useState(null);
+    const [icon, setIcon] = useState("");
+     
+    function handleResponse(response) {
+    setTemp(`${Math.round(response.data.main.temp)} °С`);
+    setTitlle(
+        `${response.data.name}, ${response.data.sys.country}:`
+    );
+    setHumidity(`Humidity:  ${response.data.main.humidity}%`);
+    setDesc(`${response.data.weather[0].description} `);
+    setWind(`Wind: ${Math.round(response.data.wind.speed)}km/h`);
+    setIcon(
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
     
- const [city, setCity] = useState("");
- const [temp, setTemp] = useState(null);
- const [titlle, setTitlle] = useState("");
- const [humidity, setHumidity] = useState(null);
- const [desc, setDesc] = useState("");
- const [wind, setWind] = useState(null);
- const [icon, setIcon] = useState("");
+    console.log(response)
 
- function handleResponse(response) {
-   setTemp(`${Math.round(response.data.main.temp)} °С`);
-   setTitlle(
-     `${response.data.name}, ${response.data.sys.country}:`
-   );
-   setHumidity(`Humidity:  ${response.data.main.humidity}%`);
-   setDesc(`${response.data.weather[0].description} `);
-   setWind(`Wind: ${Math.round(response.data.wind.speed)}km/h`);
-   setIcon(
-     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-   );
-   console.log(response)
- }
- function getApi(event) {
-   event.preventDefault();
-   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6876f80c7fdc4d4f6b847b1ddd6523b8&units=metric`;
-   axios.get(url).then(handleResponse);
- }
+    }
+    function getApi(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6876f80c7fdc4d4f6b847b1ddd6523b8&units=metric`;
+    axios.get(url).then(handleResponse);
+    }
 
- function updateCity(event) {
-   setCity(event.target.value);
- }
-
-   return (
-     <div className="container ">
-       <form className="input-city row" onSubmit={getApi}>
-         <div className="col">
-           <input
-             className="form-control shadow"
-             type="search"
-             onChange={updateCity}
-             placeholder="Type a city..."
-           />
-         </div>
-         <div className="col buttons">
-           <button type="submit" className="btn btn-search shadow">
-             Search
-           </button>
-           <button className="btn btn-current shadow">Current</button>
-         </div>
-       </form>
-       <div className="row current-city-container shadow">
-         <div className="col">
-           <h1 className="current-city">{titlle}</h1>
-           <p className="day"></p>
-         </div>
-         <div className="col">
-           <div className="weather-container">
-             <img src={icon} className="currentEmoji" alt="" width="65px" />
-             <p className="weather">{desc}</p>
-           </div>
-         </div>
-         <div className="col weather-description-container">
-           <div>
-             <h1 className="current-temp">{temp}</h1>
-             <span></span>
-           </div>
-           <div className="description-container">
-             <p className="weather-description">
-               <span>{humidity}</span> <br />
-               <span>{wind}</span>
-             </p>
-           </div>
-         </div>
-         <div className="weather-forecast"></div>
-       </div>
-       <footer className="coder">
-         This project was coded by Sofiia Andrusyshyn and is open-soursed on{" "}
-         <a
-           href="https://github.com/SofiiCode/react-weather.git"
-           target="_blank"
-           rel="noreferrer"
-         >
-           GitHub
-         </a>{" "}
-       </footer>
-     </div>
-   );
+    function updateCity(event) {
+    setCity(event.target.value);
+   
+    }
+   
+        return (
+        <div className="container ">
+        <form className="input-city row" onSubmit={getApi}>
+            <div className="col">
+            <input
+                className="form-control shadow"
+                type="search"
+                onChange={updateCity}
+                placeholder="Type a city..."
+            />
+            </div>
+            <div className="col buttons">
+            <button type="submit" className="btn btn-search shadow">
+                Search
+            </button>
+            <button className="btn btn-current shadow">Current</button>
+            </div>
+        </form>
+        <div className="row current-city-container shadow">
+            <div className="col">
+            <h1 className="current-city">{titlle}</h1>
+            <p className="day"></p>
+            </div>
+            <div className="col">
+            <div className="weather-container">
+                <img src={icon} className="currentEmoji" alt="" width="65px" />
+                <p className="weather">{desc}</p>
+            </div>
+            </div>
+            <div className="col weather-description-container">
+            <div>
+                <h1 className="current-temp">{temp}</h1>
+                <span></span>
+            </div>
+            <div className="description-container">
+                <p className="weather-description">
+                <span>{humidity}</span> <br />
+                <span>{wind}</span>
+                </p>
+            </div>
+            </div>
+            <div className="weather-forecast"></div>
+        </div>
+        </div>
+        );
+         
 }
